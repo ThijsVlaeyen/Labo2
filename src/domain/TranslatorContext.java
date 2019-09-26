ec525787d504e34971de4cc0560fd054e7488d7a
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2019-09-26 14:22:04 +0200

    TranslatorContext update

--- src/domain/TranslatorContext.java
+++ src/domain/TranslatorContext.java
@@ -1,4 +1,33 @@
 package domain;
 
-public class TranslatorContext {
+public class TranslatorContext{
+
+   private String input;
+   private CodingStrategy codingStrategy;
+
+   public TranslatorContext(CodingStrategy coding, String text){
+      this.input = text;
+      this.codingStrategy = coding;
+   }
+
+   public void setCodingStrategy(CodingStrategy codingStrategy){
+      this.codingStrategy = codingStrategy;
+   }
+
+   public String encode(){
+      return codingStrategy.encode(input);
+   }
+
+   public String decode(){
+      return codingStrategy.decode(input);
+   }
+
+   @Override
+   public String toString(){
+      String out = "";
+      out += "\nInput text: " + input;
+      out += "\nEncoded text: " + this.encode();
+      return out;
+   }
+
 }
