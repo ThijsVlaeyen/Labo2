0291875bf465a10372f6ce3d57f18ab270d4e9da
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2019-09-26 13:57:42 +0200

    Ceasar cipher done

--- src/domain/CeasarStrategy.java
+++ src/domain/CeasarStrategy.java
@@ -2,14 +2,51 @@ package domain;
 
 public class CeasarStrategy implements CodingStrategy {
 
+   private int offset;
+
+   public CeasarStrategy(int offset){
+      this.offset = offset;
+   }
+
    @Override
    public String encode(String input) {
-      return null;
+
+      StringBuilder result = new StringBuilder();
+
+      for (int i=0; i<input.length(); i++)
+      {
+         if (Character.isUpperCase(input.charAt(i)))
+         {
+            char ch = (char)(((int)input.charAt(i) + this.offset - 65) % 26 + 65);
+            result.append(ch);
+         }
+         else
+         {
+            char ch = (char)(((int)input.charAt(i) + this.offset - 97) % 26 + 97);
+            result.append(ch);
+         }
+      }
+      return result.toString();
    }
 
    @Override
    public String decode(String input) {
-      return null;
+      StringBuilder result = new StringBuilder();
+
+      for (int i=0; i<input.length(); i++)
+      {
+         if (Character.isUpperCase(input.charAt(i)))
+         {
+            char ch = (char)(((int)input.charAt(i) + -this.offset - 65) % 26 + 65);
+            result.append(ch);
+         }
+         else
+         {
+            char ch = (char)(((int)input.charAt(i) + -this.offset - 97) % 26 + 97);
+            result.append(ch);
+         }
+      }
+      return result.toString();
    }
 
 }
