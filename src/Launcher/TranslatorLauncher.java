41e0157bbd4b668daa865296c7377cfe2e9d2241
RafaelBackx <43341347+RafaelBackx@users.noreply.github.com>
2019-09-26 13:43:51 +0200

    added launcher package

---
+++ src/Launcher/TranslatorLauncher.java
@@ -0,0 +1,10 @@
+package Launcher;
+
+import ui.TranslatorUI;
+
+public class TranslatorLauncher {
+    public static void main(String[] args) {
+        TranslatorUI ui = new TranslatorUI();
+
+    }
+}
